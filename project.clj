f1f3eae90c819cd16b4ff47a6c289ed1d1a9c21f
keran.marinov <keran.fm@gmail.com>
2017-11-04 16:39:51 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject mybank "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring "1.2.0"]
+                 [composure "1.2.3-SNAPSHOT"]
+                 ])
---
+++ src/mybank/core.clj
@@ -0,0 +1,6 @@
+(ns mybank.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
