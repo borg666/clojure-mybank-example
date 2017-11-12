e70a62217089207a45476207786cec9c880ff35c
keran.marinov <keran.fm@gmail.com>
2017-11-12 15:01:13 +0000

    first commit

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ring "0.12.1"]]
+  :ring {:handler mybank.core/app
+         :auto-reload? true
+         :auto-refresh? false}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring "1.2.0"]
-                 [composure "1.2.3-SNAPSHOT"]
+                 [ring "1.6.3"]
+                 [ring/ring-json "0.5.0-beta1"]
+                 [hiccup/hiccup "2.0.0-alpha1"]
+                 [compojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "0.6.0"]
                  ])
--- src/mybank/core.clj
+++ src/mybank/core.clj
@@ -1,6 +1,54 @@
-(ns mybank.core)
+(ns mybank.core
+  (:use compojure.core
+        ring.middleware.json
+        ring.util.response)
+  (:require [compojure.route :as route]
+            [mybank.view :as view]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            )
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn makeHttpRequest
+  [url input]
+  (:body
+    (client/get url
+      {:query-params {"email" input}})))
+
+(defn extractJson
+  [responseBody]
+  (get (json/read-str responseBody) "email"))
+
+(defn getIt [url input]
+  (extractJson
+    (makeHttpRequest url input)))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (getIt "http://localhost:3000/rest" "haha")})
+
+(defn findUser [request]
+  {:status 200
+   :query-params {"name" "inputName"}
+   :headers {"Content-Type" "text/html"}
+   :body (response {:name1 name})})
+
+(defroutes my_routes
+  (GET "/" [] (view/index-page) )
+  (GET "/user" {{:keys [user-id]} :session}
+    (str "The current user is " user-id))
+  (GET "/view-request" request (str :query-string) )
+  (GET "/rest" [] (response {:email "sven@malvik.de"}))
+  (GET "/ip" [] (handler "hey"))
+  (GET "/test" [par] (response {:par1 par, :par2 "hey"}))
+  (GET "/find/:name" [name] (response {:username name}))
+  (GET "/foo" request (interpose ", " (keys request)))
+  (GET "/hello/:name" [name] (str "Hello " name))
+  (GET "/user/:id" [id]
+    (str "<h1>Hello user " id "</h1>"))
+  (route/resources "/"))
+
+
+
+(def app (wrap-json-response my_routes))
